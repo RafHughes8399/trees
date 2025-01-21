2bd1eead554f1b4b8c5ab2ffcf443a382c16afec
raf-hughes <rafhughes@gmail.com>
2025-01-21 15:11:37 +1100

    Add project files.

---
+++ binary_search_tree/binary_search_tree.cpp
@@ -0,0 +1,12 @@
+﻿// binary_search_tree.cpp : Defines the entry point for the application.
+//
+
+#include "binary_search_tree.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ binary_search_tree/binary_search_tree.h
@@ -0,0 +1,8 @@
+﻿// binary_search_tree.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
